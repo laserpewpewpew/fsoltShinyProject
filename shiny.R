5f9e0c52197c8d23e179d1d4eb0be66ee43dc81d
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-06-07 14:33:31 -0500

    KJG 6/7/2014
    First app

---
+++ shiny.R
@@ -0,0 +1,12 @@
+####################################################
+# Shiny App - Inequality Dataset Visualization App #
+# Fred Solt's Data - written by Kellen Gracey      #
+# Department of Political Science                  #
+# University of Iowa                               #
+####################################################
+library(foreign)
+library(shiny)
+library(car)
+library(ggplot2) 
+
+runApp("C:\Users\kellengracey\Documents\GitHub\fsoltShinyProject")
