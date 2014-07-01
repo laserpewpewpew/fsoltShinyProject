eb9b306ce242e46b57e50a7a6a46a48c6e1e6c59
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-07-01 11:18:38 -0500

    Added additional 2 series
    7/1/2014

--- my_app/server.R
+++ my_app/server.R
@@ -7,24 +7,7 @@ swiid <- read.csv("SWIIDv5_0summary.csv", as.is=T)
 shinyServer(function(input, output) {
 
   
-  
   output$plot <- renderPlot({
-    
-    country1 <- switch(input$country1,
-                       "Canada"="Canada")
-    
-    country2 <- switch(input$country2,
-                       "United States"="United States")
-    
-    country3 <- switch(input$country3,
-                       "United Kingdom"="United Kingdom")
-    
-    country4 <- switch(input$country4,
-                       "Germany"="Germany")
-    
-    series <- switch(input$series,
-                     "gini_net" = "gini_net",
-                     "gini_market" = "gini_market")
     
     if(input$country1=="Not Selected"){
       data.1 <- data.frame(swiid[swiid$country=="Not Selected",])
@@ -68,7 +51,7 @@ shinyServer(function(input, output) {
         labs(x = "Year", y = "SWIID Gini Index, Net Income")
       )
     }
-    else{
+    else if(input$series == "market_net"){
       print(
         ggplot(total, aes(x=year, y=gini_market, colour=country)) + 
           geom_line() +
@@ -77,6 +60,26 @@ shinyServer(function(input, output) {
           coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
           labs(x = "Year", y = "SWIID Gini Index, Market Income")
         )
+    }
+    else if(input$series == "rel_red"){
+      print(
+        ggplot(total, aes(x=year, y=rel_red, colour=country)) + 
+          geom_line() +
+          geom_ribbon(aes(ymin = rel_red-1.96*rel_red_se, ymax = rel_red+1.96*rel_red_se, 
+                          fill=country, linetype=NA), alpha = .25) +
+          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
+          labs(x = "Year", y = "SWIID Gini Index, Relative Redistribution")
+      )
+    }
+    else if(input$series == "abs_red"){
+      print(
+        ggplot(total, aes(x=year, y=abs_red, colour=country)) + 
+          geom_line() +
+          geom_ribbon(aes(ymin = abs_red-1.96*abs_red_se, ymax = abs_red+1.96*abs_red_se, 
+                          fill=country, linetype=NA), alpha = .25) +
+          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
+          labs(x = "Year", y = "SWIID Gini Index, Absolute Redistribution")
+      )
     }
     
   })
--- my_app/ui.R
+++ my_app/ui.R
@@ -10,7 +10,9 @@ shinyUI(fluidPage(
       
                  selectInput("series", label="Series",
                              choices = list("Net Inequality" = "gini_net",
-                                            "Market Inequality" = "gini_market"),
+                                            "Market Inequality" = "gini_market",
+                                            "Relative Redistribution" = "rel_red",
+                                            "Absolute Redistribution" = "abs_red"),
                              selected = "gini_net"),
                  
                  
@@ -19,16 +21,19 @@ shinyUI(fluidPage(
                  helpText("Choose up to 4 Countries:"),
                  selectInput("country1", label="Country 1",
                              choices = swiid$country,
-                             selected = "Canada"),
+                             selected = sample(1:170,1)),
+                 
                  selectInput("country2", label="Country 2",
                              choices = swiid$country,
-                             selected = "United States"),
+                             selected = sample(1:170,1)),
+                 
                  selectInput("country3", label="Country 3",
                              choices = swiid$country,
-                             selected = "United Kingdom"),
+                             selected = sample(1:170,1)),
+                 
                  selectInput("country4", label="Country 4",
                              choices = swiid$country,
-                             selected = "Germany"),
+                             selected = sample(1:170,1)),
                  
                  
                  br(),
