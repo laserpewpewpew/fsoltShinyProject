19180639b7df592ea5e6a701b45287ca4061614f
fsolt <frederick-solt@uiowa.edu>
2014-07-25 09:08:51 -0500

    implemented dynamic country lists and series; working on layout

--- my_app/server.R
+++ my_app/server.R
@@ -1,201 +1,133 @@
 library(shiny)
-library(foreign)
-library(car)
 library(ggplot2)
-library(gridExtra)
-library(lattice)
+library(reshape2)
 swiid <- read.csv("SWIIDv5_0summary.csv", as.is=T)
-compare_options <- c("within one country","across multiple countries","not selected")
+ch4 <- list("Net Inequality" = "gini_net",
+            "Market Inequality" = "gini_market",
+            "Relative Redistribution" = "rel_red",
+            "Absolute Redistribution" = "abs_red")
+ch2 <- list("Net Inequality" = "gini_net",
+            "Market Inequality" = "gini_market")
+cc <- ddply(swiid, .(country), summarize, ch = ifelse(sum(!is.na(rel_red))>0, "ch4", "ch2" ))
 
-
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
+  observe({
+    updateSelectInput(session, "country1", choices = c("select a country", cc$country), selected = "select a country")
+  })
+  observe({
+    updateSelectInput(session, "country2", choices = c("select a country", cc$country), selected = "select a country")
+  })
+  observe({
+    updateSelectInput(session, "country3", choices = c("select a country", cc$country), selected = "select a country")
+  })
+  observe({
+    updateSelectInput(session, "country4", choices = c("select a country", cc$country), selected = "select a country")
+  })
+  
+  observe({
+    if(input$country1 != "select a country") {
+      cc1 = cc[cc==input$country1, "ch"]
+    } else cc1 <- "ch2"
+    updateSelectInput(session, "series1", choices = get(cc1), selected="gini_net" )    
+  })
+  
+  
+  observe({
+    if(input$country2 != "select a country") {
+      cc2 = cc[cc==input$country2, "ch"]
+    } else cc2 <- "ch2"
+    updateSelectInput(session, "series2", choices = get(cc2), selected="gini_net" )    
+  })
+  
+  
+  observe({
+    if(input$country3 != "select a country") {
+      cc3 = cc[cc==input$country3, "ch"]
+    } else cc3 <- "ch2"
+    updateSelectInput(session, "series3", choices = get(cc3), selected="gini_net" )    
+  })
+  
+  observe({
+    if(input$country4 != "select a country") {
+      cc4 = cc[cc==input$country4, "ch"]
+    } else cc4 <- "ch2"
+    updateSelectInput(session, "series4", choices = get(cc4), selected="gini_net" )    
+  })
   
   output$plot <- renderPlot({
-    if(input$compare=="Across Multiple Countries"){
-      if(input$country1=="Not Selected"){
-        data.1 <- data.frame(swiid[swiid$country=="Not Selected",])
-        } else{
-          data.1 <- data.frame(swiid[swiid$country==input$country1,])
-        }
+    if(input$country1 != "select a country"){     
+      s1 <- data.frame(swiid[swiid$country==input$country1, 
+                             c("country", "year", input$series1, paste0(input$series1, "_se"))])
       
-      if(input$country2=="Not Selected"){
-        data.2 <- data.frame(swiid[swiid$country=="Not Selected",])
-        } else{
-          data.2 <- data.frame(swiid[swiid$country==input$country2,])
-        }
-      
-      if(input$country3=="Not Selected"){
-        data.3 <- data.frame(swiid[swiid$country=="Not Selected",])
-        } else{
-          data.3 <- data.frame(swiid[swiid$country==input$country3,])
-        }
-      
-      if(input$country4=="Not Selected"){
-        data.4 <- data.frame(swiid[swiid$country=="Not Selected",])
-        } else{
-          data.4 <- data.frame(swiid[swiid$country==input$country4,])
-        }
-      
-      data.5 <- rbind(data.1,data.2)
-      data.6 <- rbind(data.3,data.4)
-      total <- rbind(data.5,data.6)
-      
-      
-      if(input$series == "gini_net"){
-        print(arrangeGrob(
-          ggplot(total, aes(x=year, y=gini_net, colour=country)) + 
-            geom_line() +
-            geom_ribbon(aes(ymin = gini_net-1.96*gini_net_se, ymax = gini_net+1.96*gini_net_se, 
-                            fill=country, linetype=NA), alpha = .25) +
-            coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-            labs(x = "Year", y = "SWIID Gini Index, Net Income"),
-          sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                       gp=gpar(fontsize=10)))
-          )
-      } else if(input$series == "gini_market"){
-        print(arrangeGrob(
-          ggplot(total, aes(x=year, y=gini_market, colour=country)) + 
-            geom_line() +
-            geom_ribbon(aes(ymin = gini_market-1.96*gini_market_se, ymax = gini_market+1.96*gini_market_se, 
-                            fill=country, linetype=NA), alpha = .25) +
-            coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-            labs(x = "Year", y = "SWIID Gini Index, Market Income"),
-          sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                       gp=gpar(fontsize=10)))
-          )
-      } else if(input$series == "rel_red"){
-        total <- total[!is.na(total$rel_red),]
+      if(input$country2 != "select a country"){
+        s2 <- data.frame(swiid[swiid$country==input$country2, 
+                               c("country", "year", input$series2, paste0(input$series2, "_se"))])
+        s1 <- merge(s1, s2, all=T)
         
-        if(length(total$country)<=1){
-          print("You have selected a country for which redistribution data is currently unavailable.  Please select a country for which redistribution data is available.")
-          } else{      
-            print(arrangeGrob(
-              ggplot(total, aes(x=year, y=rel_red, colour=country)) + 
-                geom_line() +
-                geom_ribbon(aes(ymin = rel_red-1.96*rel_red_se, ymax = rel_red+1.96*rel_red_se, 
-                                fill=country, linetype=NA), alpha = .25) +
-                coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-                labs(x = "Year", y = "SWIID Gini Index, Relative Redistribution"),
-              sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                           gp=gpar(fontsize=10)))
-              )}
-      } else if(input$series == "abs_red"){
-        total <- total[!is.na(total$abs_red),]
-        
-        if(length(total$country)<=1){
-          print("You have selected a country for which redistribution data is currently unavailable.  Please select a country for which redistribution data is available.")
-          } else{      
-            print(arrangeGrob(
-              ggplot(total, aes(x=year, y=abs_red, colour=country)) + 
-                geom_line() +
-                geom_ribbon(aes(ymin = abs_red-1.96*abs_red_se, ymax = abs_red+1.96*abs_red_se, 
-                                fill=country, linetype=NA), alpha = .25) +
-                coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-                labs(x = "Year", y = "SWIID Gini Index, Absolute Redistribution"),
-              sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                           gp=gpar(fontsize=10)))
-              )
-          }
+        if(input$country3 != "select a country"){
+          s2 <- data.frame(swiid[swiid$country==input$country3, 
+                                 c("country", "year", input$series3, paste0(input$series3, "_se"))])
+          s1 <- merge(s1, s2, all=T)
+          if(input$country4 != "select a country"){
+            s2 <- data.frame(swiid[swiid$country==input$country4, 
+                                   c("country", "year", input$series4, paste0(input$series4, "_se"))])
+            s1 <- merge(s1, s2, all=T)
+          }   
+        } 
       }
+      
+      s1 <- melt(s1, id.vars=c("country", "year"), na.rm=T)
+      s2 <- s1[grepl("\\_se", s1$variable), c("country", "year", "value")]
+      s1 <- s1[!grepl("\\_se", s1$variable), ]
+      s1 <- cbind(s1, s2[, 3])
+      names(s1)[5] <- "value_se"
+      s1$variable <- gsub("gini_net", "Gini Index, Net Income", s1$variable)
+      s1$variable <- gsub("gini_market", "Gini Index, Market Income", s1$variable)
+      s1$variable <- gsub("rel_red", "Relative Redistribution", s1$variable)
+      s1$variable <- gsub("abs_red", "Absolute Redistribution", s1$variable)
+      if (length(table(s1$variable))==1) {
+        ylabel <- paste("SWIID", s1$variable[1])
+        s1$series <- s1$country
+      } else ylabel <- ""
+      if (length(table(s1$country))==1) {
+        c.title <- paste("Inequality in", s1$country[1])
+        s1$series <- s1$variable
+      } else c.title <- "Legend"
+      if (length(table(s1$variable))>1 & length(table(s1$country))>1) {
+        s1$series <- paste(s1$country, s1$variable, sep=", ")
+      }
+      if (input$bw==FALSE) {
+        print(arrangeGrob(
+          ggplot(s1, aes(x=year, y=value, colour=series)) + 
+            geom_line() +
+            geom_ribbon(aes(ymin = value-1.96*value_se, ymax = value+1.96*value_se, 
+                            fill=series, linetype=NA), alpha = .25) +
+            coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
+            labs(x = "Year", y = ylabel) + 
+            scale_fill_discrete(name = c.title) + scale_colour_discrete(name = c.title),
+          sub=textGrob("Source: Standardized World Income Inequality Database v5.0", x=0, hjust=-0.1, vjust=0.1, 
+                       gp=gpar(fontsize=10)))      
+        )
+      } else {
+        print(arrangeGrob(
+          ggplot(s1, aes(x=year, y=value, colour=series)) + 
+            geom_line() +
+            geom_ribbon(aes(ymin = value-1.96*value_se, ymax = value+1.96*value_se, 
+                            fill=series, linetype=NA), alpha = .25) +
+            coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
+            labs(x = "Year", y = ylabel) + 
+            scale_fill_discrete(name = c.title) + scale_colour_discrete(name = c.title) +
+            theme_bw(),
+          sub=textGrob("Source: Standardized World Income Inequality Database v5.0", x=0, hjust=-0.1, vjust=0.1, 
+                       gp=gpar(fontsize=10))) 
+        )
+      }
+       
     } else {
       return()
     }
     
   })
   
-  output$plot1 <- renderPlot({
-    if(input$compare=="Within One Country") {
-    if(input$net) {
-      data <- swiid[swiid$country==input$country,]
-      
-      print(arrangeGrob(
-        ggplot(data, aes(x=year, y=gini_net), colour=country) + 
-          geom_line() +
-          geom_ribbon(aes(ymin = gini_net-1.96*gini_net_se, ymax = gini_net+1.96*gini_net_se, 
-                          linetype=NA), alpha = .25) +
-          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-          labs(x = "Year", y = "SWIID Gini Index, Net Income"),
-        sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                     gp=gpar(fontsize=10)))
-      )
-    } else {
-      return()
-    }
-    } else {
-      return()
-    }
-    
-  })
-  
-  output$plot2 <- renderPlot({
-    if(input$compare=="Within One Country") {
-    if(input$market) {
-      data <- swiid[swiid$country==input$country,]
-      
-      print(arrangeGrob(
-        ggplot(data, aes(x=year, y=gini_market), colour=country) + 
-          geom_line() +
-          geom_ribbon(aes(ymin = gini_market-1.96*gini_market_se, ymax = gini_market+1.96*gini_market_se, 
-                          linetype=NA), alpha = .25) +
-          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-          labs(x = "Year", y = "SWIID Gini Index, Market Income"),
-        sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                     gp=gpar(fontsize=10)))
-      )
-    } else {
-      return()
-    }
-    } else {
-      return()
-    }
-  })
-  
-  output$plot3 <- renderPlot({
-    if(input$compare=="Within One Country") {
-    if(input$relative) {
-      data <- swiid[swiid$country==input$country,]
-      
-      print(arrangeGrob(
-        ggplot(data, aes(x=year, y=rel_red), colour=country) + 
-          geom_line() +
-          geom_ribbon(aes(ymin = rel_red-1.96*rel_red_se, ymax = rel_red+1.96*rel_red_se, 
-                        linetype=NA), alpha = .25) +
-          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-          labs(x = "Year", y = "SWIID Gini Index, Relative Redistribution"),
-        sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                     gp=gpar(fontsize=10)))
-      )
-    } else {
-      return()
-    }
-    } else {
-      return()
-    }
-  })
-  
-  output$plot4 <- renderPlot({
-    if(input$compare=="Within One Country") {
-    if(input$absolute) {
-      data <- swiid[swiid$country==input$country,]
-      
-      print(arrangeGrob(
-        ggplot(data, aes(x=year, y=abs_red), colour=country) + 
-          geom_line() +
-          geom_ribbon(aes(ymin = abs_red-1.96*abs_red_se, ymax = abs_red+1.96*abs_red_se, 
-                          linetype=NA), alpha = .25) +
-          coord_cartesian(xlim=c(input$dates[1],input$dates[2])) +
-          labs(x = "Year", y = "SWIID Gini Index, Absolute Redistribution"),
-        sub=textGrob("Source: Standardized World Income Inequality Database", x=0, hjust=-0.1, vjust=0.1,
-                     gp=gpar(fontsize=10)))
-      )
-    } else {
-      return()
-    }
-    } else {
-      return()
-    }
-  })
-    
-    
-}
-)
+})
--- my_app/ui.R
+++ my_app/ui.R
@@ -1,89 +1,58 @@
 library(shiny)
-library(foreign)
-swiid <- read.csv("SWIIDv5_0summary.csv", as.is=T)
 
-shinyUI(fluidPage(
-  titlePanel("Graphing SWIID"),
-  
+shinyUI(fluidPage(  
   sidebarLayout(position = "right",
-      sidebarPanel(          
-      selectInput("compare", label="Comparison:",
-                  choices=list("Within One Country" ="Within One Country",
-                               "Across Multiple Countries"="Across Multiple Countries",
-                               "Not Selected" = "Not Selected"),
-                  selected = "Not Selected"),
-      
-      conditionalPanel(
-        condition = "input.compare == 'Within One Country'",
-        checkboxInput("net", "Net Inequality",
-                      value = TRUE),
-        checkboxInput("market", "Market Inequality",
-                      value = FALSE),
-        checkboxInput("relative", "Relative Redistribution",
-                      value = FALSE),
-        checkboxInput("absolute", "Absolute Redistribution",
-                      value = FALSE),
-
-        selectInput("country", "Choose Country:",
-                    choices=swiid$country,
-                    selected=sample(1:170,1)),
-        sliderInput("dates", label="Dates",
-                    min = min(swiid$year, na.rm=T), 
-                    max = max(swiid$year, na.rm=T), 
-                    value = c(1975, max(swiid$year, 
-                                        na.rm=T)), 
-                    format = "####")
-      ),
-      
-      conditionalPanel(
-        condition = "input.compare == 'Across Multiple Countries'",
-        selectInput("series", label="Series",
-                    choices = list("Net Inequality" = "gini_net",
-                                   "Market Inequality" = "gini_market",
-                                   "Relative Redistribution" = "rel_red",
-                                   "Absolute Redistribution" = "abs_red"),
-                    selected = "gini_net"),
-        
-        br(),
-        helpText("Choose up to 4 Countries:"),
-        selectInput("country1", label="Country 1",
-                    choices = swiid$country,
-                    selected = 28),
-        
-        selectInput("country2", label="Country 2",
-                    choices = swiid$country,
-                    selected = "Not Selected"),
-        
-        selectInput("country3", label="Country 3",
-                    choices = swiid$country,
-                    selected = "Not Selected"),
-        
-        selectInput("country4", label="Country 4",
-                    choices = swiid$country,
-                    selected = "Not Selected"),
-        
-        br(),
-        helpText("Choose a Range of Dates:"),
-        sliderInput("dates", label="Dates",
-                    min = min(swiid$year, na.rm=T), max = max(swiid$year, na.rm=T), value = c(1975, max(swiid$year, na.rm=T)), format = "####")
-        ),
-      
-      conditionalPanel(
-        condition = "input.compare == 'Not Selected'",
-        helpText("Please select a comparison above.")
-        )
-      ),
-      
-      mainPanel(
-        conditionalPanel(
-          condition = "input.compare == 'Within One Country'",
-          plotOutput("plot1"),
-          plotOutput("plot2"),
-          plotOutput("plot3"),
-          plotOutput("plot4")),
-        conditionalPanel(
-          condition = "input.compare == 'Across Multiple Countries'",
-          plotOutput("plot")
-        )
-        )
-)))
+                sidebarPanel(          
+                  
+                  helpText("Choose up to 4:"),
+                  selectInput("country1", label="Country", "select a country"),
+                                    
+                  conditionalPanel(
+                    condition = "input.country1 != 'select a country'",
+                    selectInput("series1", label="Variable", 
+                                choices = list("Net Inequality" = "gini_net"), 
+                                selected = "gini_net"),
+                    
+                    selectInput("country2", label="Country2", "select a country")
+                  ),
+                  
+                  conditionalPanel(
+                    condition = "input.country2 != 'select a country'",
+                    selectInput("series2", label="Variable",
+                                choices = list("Net Inequality" = "gini_net"), 
+                                selected = "gini_net"),
+                    
+                    selectInput("country3", label="Country3", "select a country")
+                  ),
+                  
+                  conditionalPanel(
+                    condition = "input.country3 != 'select a country'",
+                    selectInput("series3", label="Variable",
+                                choices = list("Net Inequality" = "gini_net",
+                                               "Market Inequality" = "gini_market",
+                                               "Relative Redistribution" = "rel_red",
+                                               "Absolute Redistribution" = "abs_red"),
+                                selected = "gini_net"),
+                    
+                    selectInput("country4", label="Country4", "select a country")
+                  ), 
+                  
+                  conditionalPanel(
+                    condition = "input.country4 != 'select a country'",
+                    selectInput("series4", label="Variable",
+                                choices = list("Net Inequality" = "gini_net"),
+                                selected = "gini_net")
+                  ),
+                  br(),
+                  sliderInput("dates", label="Select Years:",
+                              min = min(swiid$year, na.rm=T), max = max(swiid$year, na.rm=T), 
+                              value = c(1975, max(swiid$year, na.rm=T)), format = "####"),
+                
+                  checkboxInput("bw", "Black and White Background", FALSE)
+                  ),
+                          
+                mainPanel(
+                  plotOutput("plot")
+                )
+  )
+))
